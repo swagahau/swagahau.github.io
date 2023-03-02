8c612e0d12dd8c263238af5d3aebe236c3710e4d
swagahau <96138795+swagahau@users.noreply.github.com>
2023-03-02 17:43:29 -0600

    Add files via upload

---
+++ data/story.js
@@ -0,0 +1,48 @@
+var story = {
+ "docName": "Crypto Música Landingpage",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": true,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "0:3",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Desktop - 1",
+   "image": "desktop-1.png",
+   "imageFull": "desktop-1.png",
+   "index": 0,
+   "width": 1440,
+   "height": 5108,
+   "x": -720,
+   "y": -513,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "0:1",
+   "name": "Page 1"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 1,
+ "backColor": "#FFFFFF"
+}
